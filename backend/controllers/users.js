a9c92ab7b0643ab41e2e3a6bad4c9f64e584faa7
olkolesnikova90 <olkolesnikova90@yandex.ru>
2023-09-21 13:46:17 +0500

    1

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -128,7 +128,7 @@ const login = (req, res, next) => {
             NODE_ENV === 'production' ? JWT_SECRET : 'secret-code',
           );
           res.cookie('jwt', token, {
-            domain: 'mesto-project.nomoredomainsrocks.ru',
+            domain: '.mesto-project.nomoredomainsrocks.ru',
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? true : 'none',
           });
